27223ee3b46194c0bdaf0623968144c1da367932
johns <johns@DESKTOP-A7L94PD>
2020-09-01 12:10:56 +1000

    First Commit

---
+++ src/main/java/com/johnson/spring/datajpa/model/Tutorial.java
@@ -0,0 +1,29 @@
+package com.johnson.spring.datajpa.model;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "published")
+	private boolean published;
+
+	
+}
---
+++ src/test/java/com/johnson/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnson.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
